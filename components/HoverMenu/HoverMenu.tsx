f33326082d2b2c72aa36b3bd68694095fac55e57
Pasztor Isti <isticsek@gmail.com>
2023-11-11 11:37:08 +0200

    style update

--- components/HoverMenu/HoverMenu.tsx
+++ components/HoverMenu/HoverMenu.tsx
@@ -12,7 +12,9 @@ export default function HoverMenu({ text, className, content }: Props) {
        text-orange-600 hover:text-stone-900 relative overflow-hidden group py-10 px-5`}
     >
       <div className="flex items-center justify-between gap-10">
-        <div className="relative z-[1] text-6xl font-bold">{text}</div>
+        <div className="relative z-[1] text-4xl tracking-tighter font-black">
+          {text}
+        </div>
         <div className="relative font-light text-base z-[1] delay-200 transition-all text-transparent group-hover:text-stone-100/75">
           {content}
         </div>
