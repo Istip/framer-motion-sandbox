ba3277b512a0d4b993bc5b53deca3314d53c335d
Pasztor Isti <isticsek@gmail.com>
2023-11-12 12:58:01 +0200

    update test data ids

--- data/hoverMenu.ts
+++ data/hoverMenu.ts
@@ -1,6 +1,6 @@
 export const hoverMenu = [
   { id: 1, text: "About", content: "Explore our team" },
-  { id: 1, text: "Contact", content: "Get in touch now" },
-  { id: 1, text: "Services", content: "Learn more about your options" },
-  { id: 1, text: "Work", content: "Check out our work" },
+  { id: 2, text: "Contact", content: "Get in touch now" },
+  { id: 3, text: "Services", content: "Learn more about your options" },
+  { id: 4, text: "Work", content: "Check out our work" },
 ];
