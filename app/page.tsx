023a47cecab95ab5535fc341b6a2450a836bb970
Pasztor Isti <isticsek@gmail.com>
2023-11-12 10:18:17 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
+import DoubleButton from "@/components/DoubleButton/DoubleButton";
+import Section from "@/components/Section/Section";
 import DoubleIconButtonApp from "@/sections/DoubleIconButtonApp";
 import MenuApp from "@/sections/MenuApp";
 
 export default function Home() {
   return (
-    <>
-      <DoubleIconButtonApp />
+    <div className="bg-stone-700">
+      <Section className="bg-stone-800 flex items-center justify-center">
+        <div>
+          <DoubleButton className="bg-stone-900 text-stone-500">
+            Check more
+          </DoubleButton>
+          <br />
+          <DoubleButton left className="bg-sky-900 text-sky-200">
+            Visti page
+          </DoubleButton>
+        </div>
+      </Section>
       <MenuApp />
-    </>
+    </div>
   );
 }
